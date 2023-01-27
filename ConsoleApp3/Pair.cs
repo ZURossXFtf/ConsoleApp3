26d60da98fa8144904ed226ac810eb899a03ad83
sempai <sempai@DESKTOP-2A94TS2>
2023-01-27 14:09:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Pair.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    using ConsoleApp3;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace abs
+    {
+        abstract class Pair
+        {
+            private double a;
+            private double b;
+            public double A
+            {
+                get { return a; }
+                set { a = value; }
+            }
+            public double B
+            {
+                get { return b; }
+                set { b = value; }
+            }
+            public Pair(double a, double b)
+            {
+                A = a;
+                B = b;
+            }
+            abstract public Pair Add(Pair p);
+            abstract public Pair Sub(Pair p);
+            abstract public Pair Mult(Pair p);
+            abstract public Pair Div(Pair p);
+            abstract public bool Equ(Pair p);
+            abstract public bool Great(Pair p);
+            abstract public bool Less(Pair p);
+            abstract public Pair Conj();
+
+        }
+    }
+}
---
+++ ConsoleApp3/Rational.cs
@@ -0,0 +1,73 @@
+﻿using MyApp.abs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Rational : Pair
+
+    {
+        public Rational(double a, double b) : base(a, b)
+        {
+        }
+
+        public override Pair Add(Pair p)
+        {
+            return new Rational(this.A*p.B+p.A*this.B,p.A*p.B);
+        }
+
+        public override Pair Sub(Pair p)
+        {
+            return new Rational(this.A * p.B-p.A*this.B,p.A*p.B);
+        }
+        public override Pair Mult(Pair p)
+        {
+            return new Rational(this.A * this.B, p.A * p.B);
+        }
+
+        public override Pair Div(Pair p)
+        {
+            return new Rational(this.A * p.B,p.A*this.B);
+        }
+        public override bool Equ(Pair p)
+        {
+            return (this.A == this.B) && (p.A == p.B);
+        }
+        public override bool Great(Pair p)
+        {
+            return (this.A >= this.B) && (p.A >= p.B);
+        }
+        public override bool Less(Pair p)
+        {
+            return (this.A <= this.B) && (p.A <= p.B);
+        }
+
+
+        public override Pair Conj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string? ToString()
+        {
+            if (B > 0)
+                return A + "+" + B + "i";
+            else if (B < 0)
+                return A + "-" + Math.Abs(B) + "i";
+            return A.ToString();
+        }
+        
+        
+    }
+}
